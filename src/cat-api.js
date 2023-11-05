54982b73c5507a248c7774eda110090036d121af
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-11-06 00:19:40 +0100

    00-10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,1 +1,19 @@
 import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_70dUkozJaKRJH6v6FEFfisLMREnwDOgTiuAgxgnEEsMXPrnFGZVgsw5q9FYgyhoc";
+
+export async function fetchBreeds() {
+    const response = await fetch('https://api.thecatapi.com/v1/breeds');
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+}
+
+export async function fetchCatByBreed(breedId) {
+    const response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+}
