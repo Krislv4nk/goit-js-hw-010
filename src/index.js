22b26c7f086c9ff86a52aaedaf37fab84d5f33c4
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-11-06 00:42:52 +0100

    00-42

--- src/index.js
+++ src/index.js
@@ -18,144 +18,59 @@ module.exports = {
   };
 
 
-  Ви запитали мене про те, як реалізувати деякі функції для вашого веб-сайту про котів. Я спробую допомогти вам з цим. Ось мої відповіді:
+  const loader = document.querySelector('p.loader');
+  const select = document.querySelector('select.breed-select');
+  const info = document.querySelector('div.cat-info');
+  
+  
+  loader.classList.remove('hidden');
+  select.classList.add('hidden');
+  info.classList.add('hidden');
+  
+  
+  const error = document.querySelector('p.error');
+  
+  
+  error.classList.add('hidden');
+  
+  
+  fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+     
+    })
+    .catch(err => {
+      
+      error.classList.remove('hidden');
+      error.textContent = `Error: ${err.message}`;
+    });
+  
+  error.classList.add('hidden');
+  
+  
+  fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(breeds => {
+          // Обробити відповідь
+          console.log(breeds);
+    })
+    .catch(err => {
+      
+      Notiflix.Notify.failure(`Error: ${err.message}`);
+    });
+    
+  const selectBreed = document.querySelector('select.breed-select');
+  
+  const slimSelect = new SlimSelect({
+    select: select,
+    placeholder: 'Select breed',
+    showSearch: true,
+    searchPlaceholder: 'Search by breed name',
+    searchText: 'Not found'
+  });    
 
-Показувати і приховувати завантажувач: Для того, щоб показувати і приховувати елемент p.loader в залежності від стану HTTP-запиту, ви можете використати CSS клас hidden, який встановлює властивість display на none. Ви можете додавати і видаляти цей клас за допомогою JavaScript методів classList.add() і classList.remove(). Наприклад, ви можете зробити так:
-JavaScript
-
-// Отримати елементи p.loader, select.breed-select і div.cat-info
-const loader = document.querySelector('p.loader');
-const select = document.querySelector('select.breed-select');
-const info = document.querySelector('div.cat-info');
-
-// Показати завантажувач і приховати селект і інформацію
-loader.classList.remove('hidden');
-select.classList.add('hidden');
-info.classList.add('hidden');
-
-// Приховати завантажувач і показати селект і інформацію
-loader.classList.add('hidden');
-select.classList.remove('hidden');
-info.classList.remove('hidden');
-Код, созданный ИИ. Внимательно просмотрите и используйте. Дополнительные сведения о часто задаваемых вопросах.
-Ви можете викликати ці функції перед і після виконання HTTP-запитів, щоб показувати і приховувати відповідні елементи.
-
-Опрацювання помилки: Для того, щоб відобразити і приховувати елемент p.error в разі помилки HTTP-запиту, ви можете використати той самий CSS клас hidden, який ви використовували для завантажувача. Ви також можете використати метод catch() проміса, щоб обробити помилку і показати повідомлення про помилку. Наприклад, ви можете зробити так:
-JavaScript
-
-// Отримати елемент p.error
-const error = document.querySelector('p.error');
-
-// Приховати елемент p.error
-error.classList.add('hidden');
-
-// Виконати HTTP-запит
-fetch('https://api.thecatapi.com/v1/breeds')
-  .then(response => {
-    // Обробити відповідь
-    // ...
-  })
-  .catch(err => {
-    // Показати елемент p.error і вивести повідомлення про помилку
-    error.classList.remove('hidden');
-    error.textContent = `Помилка: ${err.message}`;
-  });
-Код, созданный ИИ. Внимательно просмотрите и используйте. Дополнительные сведения о часто задаваемых вопросах.
-Ви можете використовувати цей код для будь-якого HTTP-запиту, змінюючи URL і обробку відповіді.
-
-Інтерфейс: Для того, щоб додати мінімальне оформлення елементів інтерфейсу, ви можете використати наступні кроки:
-
-Для того, щоб замінити select.breed-select на красивий селект, ви можете використати бібліотеку Slim Select. Ви повинні встановити цю бібліотеку в своєму проекті і підключити її до вашого HTML файлу. Потім ви можете використати наступний код для створення красивого селекту:
-JavaScript
-
-// Отримати елемент select.breed-select
-const select = document.querySelector('select.breed-select');
-
-// Створити красивий селект за допомогою Slim Select
-const slimSelect = new SlimSelect({
-  select: select,
-  placeholder: 'Оберіть породу кота',
-  showSearch: true,
-  searchPlaceholder: 'Пошук за назвою породи',
-  searchText: 'Нічого не знайдено'
-});
-Код, созданный ИИ. Внимательно просмотрите и используйте. Дополнительные сведения о часто задаваемых вопросах.
-Ви можете налаштувати параметри красивого селекту за вашим смаком. Детальніше про це ви можете прочитати в документації Slim Select.
-
-Для того, щоб замінити p.loader на красивий CSS-завантажувач, ви можете використати бібліотеку CSS Loaders. Ви повинні вибрати один з доступних завантажувачів і скопіювати його HTML і CSS код. Потім ви повинні вставити цей код у ваш HTML файл замість елемента p.loader. Наприклад, ви можете використати такий код для завантажувача з трьома кружечками:
-HTML
-
-<div class="loader">
-  <div class="loader__circle"></div>
-  <div class="loader__circle"></div>
-  <div class="loader__circle"></div>
-</div>
-<style>
-  .loader {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .loader__circle {
-    width: 1rem;
-    height: 1rem;
-    margin: 0 0.5rem;
-    background: #333;
-    border-radius: 50%;
-    animation: loader 0.6s linear infinite;
-  }
-
-  .loader__circle:nth-child(2) {
-    animation-delay: 0.2s;
-  }
-
-  .loader__circle:nth-child(3) {
-    animation-delay: 0.4s;
-  }
-
-  @keyframes loader {
-    0% {
-      transform: scale(0);
-    }
-    50% {
-      transform: scale(1);
-    }
-    100% {
-      transform: scale(0);
-    }
-  }
-</style>
-Код, созданный ИИ. Внимательно просмотрите и используйте. Дополнительные сведения о часто задаваемых вопросах.
-Ви можете налаштувати стилі завантажувача за вашим смаком. Детальніше про це ви можете прочитати в документації CSS Loaders.
-
-Для того, щоб замінити p.error на гарне сповіщення, ви можете використати бібліотеку Notiflix. Ви повинні встановити цю бібліотеку в своєму проекті і підключити її до вашого HTML файлу. Потім ви можете використати наступний код для показу сповіщення про помилку:
-JavaScript
-
-// Отримати елемент p.error
-const error = document.querySelector('p.error');
-
-// Приховати елемент p.error
-error.classList.add('hidden');
-
-// Виконати HTTP-запит
-fetch('https://api.thecatapi.com/v1/breeds')
-  .then(response => {
-    // Обробити відповідь
-    // ...
-  })
-  .catch(err => {
-    // Показати сповіщення про помилку за допомогою Notiflix
-    Notiflix.Notify.failure(`Помилка: ${err.message}`);
-  });
-  // Отримати елемент select.breed-select
-const select = document.querySelector('select.breed-select');
-
-// Створити красивий селект за допомогою Slim Select
-const slimSelect = new SlimSelect({
-  select: select,
-  placeholder: 'Оберіть породу кота',
-  showSearch: true,
-  searchPlaceholder: 'Пошук за назвою породи',
-  searchText: 'Нічого не знайдено'
-});
+ 
